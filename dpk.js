88037ac5dbf42c9e760ca96368306fb96f6fc16c
YavneshSharma <yavnesh.sharma@mrikal.com>
2023-03-12 10:44:32 +0530

    ClipBoard Assignment

--- dpk.js
+++ dpk.js
@@ -3,26 +3,9 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
+  const candidate = event && event.partitionKey ? event.partitionKey : crypto.createHash("sha3-512").update(JSON.stringify(event) || "").digest("hex");
+  const key = typeof candidate === "string" ? candidate : JSON.stringify(candidate);
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  return key.length > MAX_PARTITION_KEY_LENGTH ? crypto.createHash("sha3-512").update(key).digest("hex") : key || TRIVIAL_PARTITION_KEY;
 };
